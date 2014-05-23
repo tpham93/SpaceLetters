3cedbdbef59c1c499ba93da09122e932ca9ae6da
Gerd Schmidt <g-schmidt-email@gmx.de>
2014-05-23 22:24:46 +0200

    Added first part of world

---
+++ SpaceLetters/SpaceLetters/Game/World.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+
+
+namespace SpaceLetters.Game
+{
+    class World
+    {
+        List<Entity> entities = new List<Entity>();
+        
+        Sprite backgroundSprite = new Sprite(new Texture("")):
+
+        public World()
+        {
+
+
+
+        }
+
+        public void loadContent()
+        {
+
+
+        }
+
+        public void update(GameTime gameTime)
+        {
+
+
+        }
+
+        public void draw(GameTime gameTime, SFML.Graphics.RenderWindow window)
+        {
+
+        }
+    }
+}
