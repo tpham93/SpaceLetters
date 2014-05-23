675115e6bb31c74fe34ea90e70ab51820ce70ef5
Gerd Schmidt <g-schmidt-email@gmx.de>
2014-05-24 00:39:23 +0200

    Added waepons an canons do commit for yorick

---
+++ SpaceLetters/SpaceLetters/Game/Weapon/Weapon.cs
@@ -0,0 +1,52 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceLetters
+{
+    abstract class Weapon
+    {
+        private float coolDown;
+
+        protected Vec2f position;
+        protected float rotation;
+        protected float radius;
+        protected Sprite sprite;
+
+        protected float runCoolDownTime;
+
+         public Weapon(Vec2f position, float rotation, float radius, Sprite sprite)
+        {
+            this.position = position;
+            this.rotation = rotation;
+            this.radius = radius;
+            this.sprite = sprite;
+
+            initialize();
+            
+        }
+
+
+
+        public bool isCoolDownOver()
+        {
+            return runCoolDownTime >= coolDown;
+
+        }
+
+        public abstract bool fire(Vec2f target);
+
+
+
+        public abstract void initialize();
+
+        public abstract void loadContent();
+        public abstract void update(GameTime gameTime);
+
+        public abstract void draw(GameTime gameTime, RenderWindow renderWindow);
+
+    }
+}
