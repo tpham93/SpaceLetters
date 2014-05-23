54405d9af4c5df9b1881976cee6debea2129d554
Tuan Pham Minh <gearhunter@gmx.de>
2014-05-23 21:53:25 +0200

    gamestates

--- SpaceLetters/SpaceLetters/Game/Game.cs
+++ SpaceLetters/SpaceLetters/Game/Game.cs
@@ -11,23 +11,73 @@ namespace SpaceLetters
 {
     class Game : AbstractGame
     {
+        private AGameState currentGameStateObject;
+        private AGameState backedUpGameStateObject;
 
-        public Game() : base (800,480, "Epic Game", Styles.Default)
+        private EGameStates currentGameState;
+
+        private EGameStates CurrentGameState
         {
+            get { return currentGameState; }
+            set
+            {
+                if (value != currentGameState)
+                {
+                    switch (value)
+                    {
+                        case EGameStates.MainMenu:
+                            if(currentGameState == EGameStates.Pause)
+                            {
+                                currentGameStateObject = backedUpGameStateObject;
+                                backedUpGameStateObject = null;
+                            }
+                            else
+                            {
+                                backedUpGameStateObject = null;
+                                currentGameStateObject = new MainMenu();
+                            }
+                            break;
+                        case EGameStates.InGame:
+                            backedUpGameStateObject = null;
+                            //currentGameStateObject = new InGame();
+                            break;
+                        case EGameStates.Pause:
+                            backedUpGameStateObject = currentGameStateObject;
+                            //currentGameStateObject = new Pause();
+                            break;
+                        case EGameStates.Credits:
+                            backedUpGameStateObject = null;
+                            //currentGameStateObject = new Credits();
+                            break;
+                        case EGameStates.Exit:
 
+                            break;
+                    }
+                    currentGameState = value;
+                }
+            }
+        }
+
+        public Game()
+            : base(800, 480, "Epic Game", Styles.Default)
+        {
+            currentGameState = EGameStates.MainMenu;
+            currentGameStateObject = new MainMenu();
+            backedUpGameStateObject = null;
 
         }
 
 
         public override void update(GameTime gameTime)
         {
-            window.Clear(Color.Blue);
-
+            currentGameStateObject.update(gameTime);
             //throw new NotImplementedException();
         }
 
         public override void draw(GameTime gameTime, SFML.Graphics.RenderWindow window)
         {
+            window.Clear(Color.Blue);
+            currentGameStateObject.draw(gameTime, window);
             //throw new NotImplementedException();
         }
     }
---
+++ SpaceLetters/SpaceLetters/Game/GameStates/AGameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.Graphics;
+
+namespace SpaceLetters
+{
+    public abstract class AGameState
+    {
+        public abstract EGameStates getGameStateType();
+        public abstract void loadContent();
+        public abstract EGameStates update(GameTime gameTime);
+        public abstract void draw(GameTime gameTime, RenderWindow renderWindow);
+    }
+}
