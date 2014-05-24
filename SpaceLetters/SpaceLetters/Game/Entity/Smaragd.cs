bd3c650a1dffb69077969a02c1f8861c44e8d2a9
Gerd Schmidt <g-schmidt-email@gmx.de>
2014-05-24 12:38:14 +0200

    Added Smaragd.cs

---
+++ SpaceLetters/SpaceLetters/Game/Entity/Smaragd.cs
@@ -0,0 +1,46 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceLetters
+{
+    class Smaragd : Entity
+    {
+        private static Texture texture = new Texture("Content/InGame/LetterCase/smara.png");
+
+
+        public Smaragd(Vec2f position, float rotation, float hp, float radius, Vec2f velocity, Team team, String name)
+            :base(position, rotation, hp, radius, velocity, team, name,new Sprite(texture))
+        {
+
+        }
+
+        public override EntityType getEntityType()
+        {
+            return EntityType.Letter;
+        }
+
+        public override void initialize()
+        {
+
+        }
+
+        public override void update(GameTime gameTime)
+        {
+          
+        }
+
+        public override void draw(GameTime gameTime, SFML.Graphics.RenderWindow renderWindow)
+        {
+          
+        }
+
+        public override void loadContent()
+        {
+           
+        }
+    }
+}
