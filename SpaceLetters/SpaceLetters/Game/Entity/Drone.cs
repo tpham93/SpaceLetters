5ec860d69cb416c343a9fe3d3f5575867d25d596
yorick-ne <yoyo.bokel@gmx.de>
2014-05-24 12:23:09 +0200

    drone

---
+++ SpaceLetters/SpaceLetters/Game/Entity/Drone.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.Graphics;
+
+namespace SpaceLetters
+{
+    class Drone : Entity
+    {
+        public Drone(Vec2f position, float rotation, float radius, float hp, Vec2f velocity, Team team, String name, Sprite sprite)
+            :base(position, rotation,  radius,  hp,  velocity,  team,  name,  sprite)
+        {
+
+            initialize();
+        }
+
+        public override EntityType getEntityType()
+        {
+            return EntityType.Drone;
+        }
+
+        public override void initialize()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void loadContent()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void update(GameTime gameTime)
+        {
+           // throw new NotImplementedException();
+        }
+
+        public override void draw(GameTime gameTime, RenderWindow renderWindow)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
