4461e5a8aabb000f249265c3b37592d99de5d6f7
Gerd Schmidt <g-schmidt-email@gmx.de>
2014-05-24 16:37:56 +0200

    change images

--- SpaceLetters/SpaceLetters/Game/Entity/Player.cs
+++ SpaceLetters/SpaceLetters/Game/Entity/Player.cs
@@ -28,7 +28,7 @@ namespace SpaceLetters
         public Player(Vec2f position, float rotation, float hp, float radius, Vec2f velocity, Team team, String name)
             :base(position, rotation, hp, float.PositiveInfinity, radius, velocity, team, name,new Sprite(texture))
         {
-            const uint DEFAULT_WEAPON_NÙMBER = 8;
+            const uint DEFAULT_WEAPON_NÙMBER = 20;
             Texture cannonTexture = new Texture("Content/InGame/cannon.png");
             for (int i = 0; i < DEFAULT_WEAPON_NÙMBER; ++i)
             {
