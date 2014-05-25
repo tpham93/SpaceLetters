d43d487dc260e11b4ccc7d4ce593a1288ed955b1
yorick-ne <yoyo.bokel@gmx.de>
2014-05-25 11:41:41 +0200

    asdf

--- SpaceLetters/SpaceLetters/Game/Spawner.cs
+++ SpaceLetters/SpaceLetters/Game/Spawner.cs
@@ -8,7 +8,6 @@ namespace SpaceLetters
 {
     class Spawner
     {
-        private TimeSpan startTime;
         private Random rand;
         private Entity player;
         private float spawnVelocity;
@@ -33,15 +32,10 @@ namespace SpaceLetters
         }
         public Entity spawn(GameTime gameTime)
         {
-            if(startTime == null ){
-                startTime = gameTime.TotalTime;
-            }
-
             //if(rand.NextDouble()*0.91f *spawnVelocity *  Math.Max(1,gameTime.TotalTime.Minutes) > 1)
             //if(rand.NextDouble() < 0.003f)
             //if (rand.NextDouble() < 0.0025f + gameTime.TotalTime.Minutes/1000)
-            //if (rand.NextDouble() < 0.0025f + (gameTime.TotalTime.TotalSeconds % 60)/1000)
-            if((gameTime.TotalTime - startTime).TotalMilliseconds > 666)
+            if (rand.NextDouble() < 0.0025f + (gameTime.TotalTime.TotalSeconds % 60)/1000)
             {
                 
 
